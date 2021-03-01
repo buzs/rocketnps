bda18a8416f0d343a5f4d52a70c46e347d617a6e
Buz <contato@gabrielhenrique.net>
2021-03-01 11:03:36 -0300

    add user controller and router

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import "reflect-metadata"
+import express from 'express';
+
+import "./database"
+import { router } from "./routes";
+
+const port = process.env.PORT || 3333;
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
